1f36b4c6246636824571d56c5d0f80f2773a4d0c
chamara001 <116743136+chamara001@users.noreply.github.com>
2022-10-26 21:16:24 +0530

    Update Hacktomberfest_2022_PHP_Tast03.php

--- Hacktomberfest_2022_PHP_Tast03.php
+++ Hacktomberfest_2022_PHP_Tast03.php
@@ -1,10 +1,10 @@
 <?php
+// assign data
 $txt1 = "Learn PHP";
-txt2 = "W3Schools.com";
+$txt2 = "W3Schools.com";
 $x = 5;
-y = 4
-
-echo "<h2>" . txt1 . "</h2>";
+$y = 4;
+echo "<h2>" . $txt1 . "</h2>";
 echo "Study PHP at " . $txt2 . "<br>";
 echo $x + $y;
 ?>
