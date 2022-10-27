b2df264ed95eb6b351e9f699c929742e63421668
UdaraDZ <116781547+UdaraDZ@users.noreply.github.com>
2022-10-27 10:21:26 +0530

    Update Hacktomberfest_2022_PHP_Tast03.php

--- Hacktomberfest_2022_PHP_Tast03.php
+++ Hacktomberfest_2022_PHP_Tast03.php
@@ -1,10 +1,13 @@
 <?php
 $txt1 = "Learn PHP";
-txt2 = "W3Schools.com";
+//value assign
+$txt2 = "W3Schools.com";
+//value assign
 $x = 5;
-y = 4
-
-echo "<h2>" . txt1 . "</h2>";
+//value assign
+$y = 4;
+//value assign
+echo "<h2>" . $txt1 . "</h2>";
 echo "Study PHP at " . $txt2 . "<br>";
 echo $x + $y;
 ?>
